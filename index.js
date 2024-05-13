65f2fd830b68569280a3c523c5c75d4709ed42b1
loganthomasmusic <ltstewart95@gmail.com>
2024-05-13 17:14:32 -0400

    Create Express code to route endpoints

--- index.js
+++ index.js
@@ -1,50 +1,82 @@
 const express = require('express');
     morgan = require('morgan');
+    uuid = require('uuid');
 
 const app = express();
 
 app.use(morgan('common'));
+app.use(express.json());
 
-let topMovies = [
+let users = [
+  {
+    id:'1',
+    name: 'John Doe',
+    email: 'johndoe@mail.com',
+    favMovies: [{
+      title: 'Inception',
+      director: 'Christopher Nolan',
+      genre: 'Sci-Fi'
+    }]
+  },
+  {
+    id:'2',
+    name: 'Jane Doe',
+    email: 'janedoe@mail.com',
+    favMovies: [{
+      title: 'Inception',
+      director: 'Christopher Nolan',
+      genre: 'Sci-Fi'
+    }]
+  }
+
+];
+
+let movies = [
     {
-      title: 'Movie 1',
-      director: 'Director 1'
+      Title: 'Movie 1',
+      Director: {
+        Name: 'Director 1'
+      },
+      Genre: {
+          Name: 'Thriller',
+          Description: 'A genre of fiction that evokes excitement and suspense in the audience'
+      }
     },
     {
-      title: 'Movie 2',
-      author: 'Director 2'
+      Title: 'Movie 2',
+      Director: 'Director 2'
     },
     {
-      title: 'Movie 3',
-      author: 'Director 3'
+      Title: 'Movie 3',
+      Director: 'Director 3'
     },
     {
-      title: 'Movie 4',
-      director: 'Director 4'
+      Title: 'Movie 4',
+      Director: 'Director 4'
     },
     {
-      title: 'Movie 5',
-      author: 'Director 5'
+      Title: 'Movie 5',
+      Director: 'Director 5'
     },
     {
-      title: 'Movie 6',
-      author: 'Director 6'
+      Title: 'Movie 6',
+      Director: 'Director 6'
     },
     {
-      title: 'Movie 7',
-      author: 'Director 7'
+      Title: 'Movie 7',
+      Director: 'Director 7'
     },
     {
-      title: 'Movie 8',
-      director: 'Director 8'
+      Title: 'Movie 8',
+      Director: 'Director 8'
     },
     {
-      title: 'Movie 9',
-      author: 'Director 9'
+      Title: 'Movie 9',
+      Director: 'Director 9'
     },
     {
-      title: 'Movie 10',
-      author: 'Director 10'
+      Title: 'Movie 10',
+      Director: 'Director 10'
     }
   ];
   
@@ -68,3 +100,122 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
+
+//READ Return a list of ALL movies to the user
+app.get('/movies', (req, res) => {
+  res.status(200).json(movies);
+})
+
+//READ Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user
+app.get('/movies/:title', (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find( movie => movie.Title === title );
+
+  if (movie) {
+    res.status(200).json(movie); 
+  } else {
+    res.status(400).send('no such movie')
+  }
+
+})
+
+//READ Return data about a genre (description) by name/title (e.g., “Thriller”)
+app.get('/movies/genre/:genreName', (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
+
+  if (genre) {
+    res.status(200).json(genre); 
+  } else {
+    res.status(400).send('no such genre')
+  }
+
+})
+
+//READ Return data about a director (bio, birth year, death year) by name
+app.get('/movies/directors/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find( movie => movie.Director.Name === directorName ).Director;
+
+  if (director) {
+    res.status(200).json(director); 
+  } else {
+    res.status(400).send('no such director')
+  }
+
+})
+
+//CREATE Allow new users to register
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  } else {
+    res.status(400).send('users need names')
+  }
+
+})
+
+//UPDATE Allow users to update their user info (username)
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find( user => user.id == id)
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('no such user')
+  }
+
+})
+
+//CREATE Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later);
+app.post('/users/:id/:moviesTitle', (req, res) => {
+  const { id, moviesTitle } = req.params;
+
+  let user = users.find( user => user.id == id);
+
+  if (user) {
+    user.favMovies.push(moviesTitle);
+    res.status(200).send(`${moviesTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send('no such user')
+  }
+
+})
+
+//DELETE Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later);
+app.delete('/users/:id/:moviesTitle', (req, res) => {
+  const { id, moviesTitle } = req.params;
+
+  let user = users.find( user => user.id == id)
+
+  if (user) {
+    user.favMovies = user.favMovies.filter( title => title !== moviesTitle);
+    res.status(200).send(`${moviesTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send('no such user')
+  }
+
+})
+
+//DELETE Allow existing users to deregister (showing only a text that a user email has been removed—more on this later).
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find( user => user.id == id)
+
+  if (user) {
+    users = users.filter( user => user.id != id);
+    res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send('no such user')
+  }
+
+})
